2c1f136fd710325ea21098c3c816bc3a5fae1b61
T Farwell <farwelltj1988@gmail.com>
2013-09-13 16:06:20 -0400

    app.py stuff

--- app.py
+++ app.py
@@ -10,19 +10,6 @@ def hello():
 @app.route('/occs home.html')
 def assignment_1():
     return redirect(url_for('static', filename='occs home.html'))
-@app.route('')
-def assignment_3():
-    return redirect(url_for('static', filename=''))
-
-@app.route('')
-def assignment4():
-    return redirect(url_for('static', filename=''))
-@app.route('')
-def assignment5():
-    return redirect(url_for('static', filename=''))
-@app.route('')
-def assignment6():
-    return redirect(url_for('static', filename=''))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
