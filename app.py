aa3dea021b66be49cd613b6f43fa655962066f17
Ture Farwell <farwelltj1988@gmail.com>
2013-09-19 17:40:53 -0400

    changed app.py

--- app.py
+++ app.py
@@ -3,13 +3,15 @@ from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/Index.html')
+# Home page. Index link for all assignments
+@app.route('/')
 def hello():
     return redirect(url_for('static', filename='Index.html'))
 
-@app.route('/occs home.html')
-def assignment_1():
-    return redirect(url_for('static', filename='occs home.html'))
+# # home: occs home page
+# @app.route('/home')
+# def home():
+#     return redirect(url_for('static', filename='occs home.html'))
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
